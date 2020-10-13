9c7f296ddf4a6560bee16166bdabbb0500f97092
Afzal Najam <afzalnaj@gmail.com>
2020-10-13 00:09:31 -0400

    Fix imports

--- app/src/main/java/com/afzaln/funcompose/navigation/BasicNav.kt
+++ app/src/main/java/com/afzaln/funcompose/navigation/BasicNav.kt
@@ -1,8 +1,12 @@
 package com.afzaln.funcompose.navigation
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.navigation.AmbientNavController
 import androidx.compose.navigation.NavHost
 import androidx.compose.navigation.composable
+import androidx.compose.navigation.rememberNavController
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 
 /**
  * The navController is created automatically by
